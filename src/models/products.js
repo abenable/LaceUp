a247c2d88101fa72ac89c7b81f5d69729c32d87c
abenable <ableabenaitwe@gmail.com>
2024-05-15 17:42:18 +0300

    chore: Add review schema and model for products

--- src/models/products.js
+++ src/models/products.js
@@ -9,5 +9,12 @@ const productSchema = new mongoose.Schema({
     imageUrl: { type: String },
     stockQuantity: { type: Number, required: true },
 });
+const reviewSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    description: { type: String },
+    rating: { type: Number, required: true },
+    dateAdded: { type: Date, default: Date.now },
+});
 
 export const ProductModel = mongoose.model("products", productSchema);
+export const ReviewModel = mongoose.model("reviews", reviewSchema);
