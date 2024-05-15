bd60631b58c5e1dc675895df1d0631533d708c89
abenable <ableabenaitwe@gmail.com>
2024-05-15 13:35:54 +0300

    feat: Add error controller and error handling middleware

---
+++ src/controllers/error.js
@@ -0,0 +1,30 @@
+import logger from '../utils/logger.js';
+
+class ApiError extends Error {
+  constructor(statusCode, message, details = null) {
+    super(message);
+    this.statusCode = statusCode;
+    this.details = details;
+    this.name = this.constructor.name;
+    this.isOperational = true;
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+const ErrorHandler = (err, req, res, next) => {
+  let statusCode = err.statusCode || 500; // Internal Server Error code.
+  let message = err.message || 'Internal Server Error';
+  let details = err.details;
+
+  if (process.env.NODE_ENV === 'development') {
+    logger.error(err);
+  }
+
+  res.status(statusCode).json({
+    status: 'Error',
+    error_message: message,
+    ...(details && { details: details }),
+  });
+};
+
+export { ApiError, ErrorHandler };
